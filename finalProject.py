7871b3212d6ec984d88802bcefe4cff129280147
eburnette1 <16eburnette@castilleja.org>
2017-04-25 11:09:16 -0400

    Read in CSV file, was able to print her out in another file so i know she's A-ok

--- finalProject.py
+++ finalProject.py
@@ -12,5 +12,31 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 import numpy as np
+import pandas as pd
 
+def main():
+    
+    fileName = input("Input file name: ")
+    infile = open(fileName, 'r')
+    quorum_csv = csv.reader(infile)
+    next(quorum_csv) # skips first line to get rid of the headings
+    print(quorum_csv)
+    
+    #values taken from the CSV file
+    givenTime = [];
+    givenA = [];
+    givenP = [];
+    
+    for line in infile.readlines():
+        field = line.strip().split(",")
+        givenTime.append(float(field[0]))
+        givenA.append(float(field[1]))
+        givenP.append(float(field[2]))
 
+    data = {'time': givenTime, 'A' : givenA, 'P' : givenP}
+    graph = pd.DataFrame(data, columns=['time', 'A', 'P'])
+    outfile = open("quorumOutfile.csv", "w")     
+    graph.to_csv(outfile)
+    
+    infile.close();
+main();
