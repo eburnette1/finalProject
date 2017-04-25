054e62ccc7da46c9b10a6970b12f809838421b9a
eburnette1 <16eburnette@castilleja.org>
2017-04-25 10:12:59 -0400

    added CSV

--- finalProject.py
+++ finalProject.py
@@ -7,4 +7,10 @@ Created on Tue Apr 25 09:47:47 2017
 
 Final Project 3: Quorum Sensing
 """
+import csv 
+import matplotlib.pyplot as plt  
+import matplotlib.patches as mpatches
+
+import numpy as np
+
 
