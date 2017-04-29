89ea7d4d799e1f58410e06217ef17c1f01fd101b
Julia Costacurta <jcostacurta11@gmail.com>
2017-04-29 17:51:34 -0400

    starting reaction classes
    i think i did part 1
    created separate classes for just P and then both P and AI

--- finalProject.py
+++ finalProject.py
@@ -14,8 +14,22 @@ import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
 
-class reaction: #class that has properties for a single reaction
-    def __init__(self, k, n, beta, eta, epsilon, alpha): #initializing function
+class pReaction:
+    def __init__(self, P, k, n, beta, eta):
+        self.k = k
+        self.n = n
+        self.beta = beta
+        self.eta = eta
+        
+        self.P = P
+        self.AI = 0
+        
+    def update(self, time): #function to update concentrations of chemicals for a time interval (time)
+        self.P = self.P + time*(-self.eta*self.P)
+        print(self.P)
+    
+class totalReaction: #class that has properties for a reaction with both P and AI
+    def __init__(self, P, AI, k, n, beta, eta, epsilon, alpha): #initializing function
         self.k = k
         self.n = n
         self.beta = beta
@@ -23,41 +37,46 @@ class reaction: #class that has properties for a single reaction
         self.epsilon = epsilon
         self.alpha = alpha
         
-        self.P = 0
-        self.AI = 0
-
-    def setConc(self,P,AI):
         self.P = P
         self.AI = AI
-  
+
     def update(self, time): #function to update concentrations of chemicals for a time interval (time)
-        self.P = self.P + time*beta((self.AI**self.n)/(self.k**n+self.AI**n))
+        self.P = self.P + time*(self.beta*((self.AI**self.n)/(self.k**self.n+self.AI**self.n))-self.eta*self.P)
         self.AI = self.AI + time*(self.epsilon*self.P-self.alpha*self.AI)
+
+
 
 
 def main():
     
-    fileName = input("Input file name: ")
-    infile = open(fileName, 'r')
-    quorum_csv = csv.reader(infile)
-    next(quorum_csv) # skips first line to get rid of the headings
-    print(quorum_csv)
-    
-    #values taken from the CSV file
-    givenTime = [];
-    givenA = [];
-    givenP = [];
-    
-    for line in infile.readlines():
-        field = line.strip().split(",")
-        givenTime.append(float(field[0]))
-        givenA.append(float(field[1]))
-        givenP.append(float(field[2]))
-
-    data = {'time': givenTime, 'A' : givenA, 'P' : givenP}
-    graph = pd.DataFrame(data, columns=['time', 'A', 'P'])
-    outfile = open("quorumOutfile.csv", "w")     
-    graph.to_csv(outfile)
-    
-    infile.close();
+    reactOne = pReaction(1000,1*10**(-9),3,10,3)
+    reactOneValues = []
+    while reactOne.P>1:
+        reactOneValues.append(reactOne.P)
+        reactOne.update(.1)
+    plt.plot(reactOneValues)
+    print(reactOneValues)
+#    fileName = input("Input file name: ")
+#    infile = open(fileName, 'r')
+#    quorum_csv = csv.reader(infile)
+#    next(quorum_csv) # skips first line to get rid of the headings
+#    print(quorum_csv)
+#    
+#    #values taken from the CSV file
+#    givenTime = [];
+#    givenA = [];
+#    givenP = [];
+#    
+#    for line in infile.readlines():
+#        field = line.strip().split(",")
+#        givenTime.append(float(field[0]))
+#        givenA.append(float(field[1]))
+#        givenP.append(float(field[2]))
+#
+#    data = {'time': givenTime, 'A' : givenA, 'P' : givenP}
+#    graph = pd.DataFrame(data, columns=['time', 'A', 'P'])
+#    outfile = open("quorumOutfile.csv", "w")     
+#    graph.to_csv(outfile)
+#    
+#    infile.close();
 main();
