7982ed6bcdff198fbf1590bf876bc439ae9cf7bb
Julia Costacurta <jcostacurta11@gmail.com>
2017-05-12 14:59:53 -0400

    part 1 and 2 output

--- finalProject.py
+++ finalProject.py
@@ -25,7 +25,6 @@ class pReaction:
     def update(self, time): #function to update concentrations of chemicals for a time interval (time)
         self.P = self.P + time*(self.beta*((self.AI**self.n)/(self.k**self.n+self.AI**self.n))-self.eta*self.P)
         
-        print(self.P)
     
         
 class aReaction: #class that has properties for a reaction with constant P
@@ -80,29 +79,58 @@ def rss(list1,list2):
 def main():
     print("Final Project 3, by Emily Burnette, Nick Garza, and Julia Costacurta")
 #below is for part 1 of the prompt; finding a P graph without any AI feedback
-    #r1 =pReaction(1000, 0,1*10**(-9), 3,10,3)
-    #P=[]
+    r1 =pReaction(1000, 0,1*10**(-9), 3,10,3)
+    P=[]
     
-    #for i in range (101):
-       #P.append(r1.P)
-       #r1.update(.1)
-    #t = np.arange(0., 10.1, 0.1)
+    for i in range (101):
+       P.append(r1.P)
+       r1.update(.1)
+    t = np.arange(0., 10.1, 0.1)
+    
+    plt.plot(t,P)
+    plt.title('Part 1 - Just P Graph')
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Concentration (P) ")
+    plt.figure()
 
+    
 #below is for part 2 of the prompt, finding P and AI values with an epsilon of 5e-10 and alpha of .1. The independent concentration of P is commented out above so it doesn't mess with other stuff
-    reactTwo = totalReaction(5,0,10**(-9),3,10,3,5*10**(-10)) #this also doubles as low P
-    reactTwoP = []
-    reactTwoAI = []
+    reactAI = aReaction(5,0,3,5*10**(-10)) 
+    reactAIP = []
+    reactAIAI = []
 
     for i in range(101):
-        reactTwoP.append(reactTwo.P)
-        reactTwoAI.append(reactTwo.AI)
-        reactTwo.update(.1)
+        reactAIP.append(reactAI.P)
+        reactAIAI.append(reactAI.AI)
+        reactAI.update(.1)
     t = np.arange(0., 10.1, 0.1)
-
+    
+    plt.plot(t,reactAIP)
+    plt.title('Part 2 - Steady P, P Graph')
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Concentration (P) ")
+    plt.figure()
+    
+    plt.plot(t,reactAIAI)
+    plt.title('Part 2 - Steady P, AI Graph')
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Concentration (A) ")
+    plt.figure()
     
 
 # Graphing part 3
-    reactHigh = totalReaction(100,0,10**(-9),3,10,3,5*10**(-10)) 
+    reactLow = totalReaction(5,0,10**(-9),3,10,3,5*10**(-10)) #low initial P
+    reactLowP = []
+    reactLowAI = []
+
+    for i in range(101):
+        reactLowP.append(reactLow.P)
+        reactLowAI.append(reactLow.AI)
+        reactLow.update(.1)
+    t = np.arange(0., 10.1, 0.1)
+    
+    
+    reactHigh = totalReaction(100,0,10**(-9),3,10,3,5*10**(-10)) #high initial P
     reactHighP = []
     reactHighAI = []
 
@@ -113,29 +141,29 @@ def main():
     t = np.arange(0., 10.1, 0.1)
     
     #low P
-    plt.plot(t,reactTwoP)
-    plt.title('Low P Graph')
+    plt.plot(t,reactLowP)
+    plt.title('Part 3 - Low P, P Graph')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (P) ")
     plt.figure()
     
     #high P
     plt.plot(t,reactHighP)
-    plt.title('High P Graph')
+    plt.title('Part 3 - High P, P Graph')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (P) ")
     plt.figure()
     
     #low P AI graph
-    plt.plot(t,reactTwoAI)
-    plt.title('Low AI Graph')
+    plt.plot(t,reactLowAI)
+    plt.title('Part 3 - Low P, AI Graph')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (A) ")
     plt.figure()
     
     #high P AI Graph
     plt.plot(t,reactHighAI)
-    plt.title('High AI Graph')
+    plt.title('Part 3 - High P, AI Graph')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (A) ")
     
@@ -185,7 +213,7 @@ def main():
 
     
 # Finding best Epsilon value
-    best2 = rss(reactTwoAI,givenA)
+    best2 = rss(reactLowAI,givenA)
     for i in np.arange(1*10**(-11),1*10**(-9),1*10**(-12)):
         reacti = totalReaction(5,0,10**(-9),3,10,3,i)
         A = []
@@ -198,7 +226,7 @@ def main():
     print("The best epsilon is", bestEpsilon)
     
 #Finding best Beta value
-    best = rss(reactTwoP,givenP)
+    best = rss(reactLowP,givenP)
     for i in np.arange(0,20,0.1):
         reacti = totalReaction(5,0,10**(-9),3,i,3,bestEpsilon)
         P = []
@@ -225,7 +253,7 @@ def main():
 #Graph comparing given and optimized P values
     plt.figure()
     plt.plot(t, reactTotalP, 'b--', t, givenP, 'r')
-    plt.title('Given and Optimized P')
+    plt.title('Part 5 - Given and Optimized P')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (P) ")
     blue_patch = mpatches.Patch(color='blue', label='Optimized P')
@@ -236,7 +264,7 @@ def main():
 #Graph comparing given and optimized AI values    
     plt.figure()
     plt.plot(t, reactTotalAI, 'g--', t, givenA, 'r')
-    plt.title('Given and Optimized A')
+    plt.title('Part 5 - Given and Optimized A')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Concentration (A) ")
     green_patch = mpatches.Patch(color='green', label='Optimized A')
